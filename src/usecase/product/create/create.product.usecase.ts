00e8bcf5dfd91fd3cdcc9200ce4bb91ac800c8db
EurimarJorge <60715863+EurimarJorge@users.noreply.github.com>
2023-02-25 11:53:03 -0300

    Add files via upload

---
+++ src/usecase/product/create/create.product.usecase.ts
@@ -0,0 +1,28 @@
+import Product from "../../../domain/product/entity/product";
+import ProductFactory from "../../../domain/product/factory/product.factory";
+import ProductRepositoryInterface from "../../../domain/product/repository/product-repository.interface";
+import {
+  InputCreateProductDto,
+  OutputCreateProductDto,
+} from "./create.product.dto";
+
+export default class CreateCustomerUseCase {
+  private productRepository: ProductRepositoryInterface;
+
+  constructor(productRepository: ProductRepositoryInterface) {
+    this.productRepository = productRepository;
+  }
+
+  async execute(
+    input: InputCreateProductDto
+  ): Promise<OutputCreateProductDto> {
+    const p = ProductFactory.create("a",input.name, input.price);
+    await this.productRepository.create(p);
+    
+    return {
+      id: p.id,
+      name: p.name,
+      price: p.price,
+    };
+  }
+}
